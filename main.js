67dd05650e0e3479c6b946a0e98afb634ea5f22c
Scripted Adventurer <scripted.adventurer@fake.com>
2020-08-13 09:34:07 -0400

    Updating logic for unique paths.

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function main() {
   svg.setAttribute("width", userInput.canvasX);
   svg.setAttribute("height", userInput.canvasY);
   for (let path = 1; path <= userInput.numPaths; path++) {
-    let randPath = new Path(userInput.seed, userInput.canvasX, 
+    // add the path number to the seed so each path is unique
+    let randPath = new Path(userInput.seed+path, userInput.canvasX, 
       userInput.canvasY);
     randPath.buildPath();
     let pathElement = document.createElementNS("http://www.w3.org/2000/svg", 
